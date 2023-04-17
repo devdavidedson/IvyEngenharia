045eacec8197a990cfbe7107d68cc0728ae80732
David Lopes <104918465+devdavidedson@users.noreply.github.com>
2023-04-17 19:54:34 -0300

    slideATT

--- js/script.js
+++ js/script.js
@@ -13,3 +13,14 @@ const observer = new IntersectionObserver(entries => {
 Array.from(document.querySelectorAll('.init-hidden')).forEach( element => {
     observer.observe(element)   
 });
+
+const slideList = document.querySelector('.sec__slide-list');
+const slides = slideList.querySelectorAll('li');
+let currentSlide = 0;
+let slideInterval = setInterval(nextSlide, 5000);
+
+function nextSlide() {
+  slides[currentSlide].style.opacity = 0;
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].style.opacity = 1;
+}
